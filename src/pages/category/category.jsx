54c3ebefa1d1495135dbb691c4a43ff1212a24e3
zhengqixiang <937084889@qq.com>
2021-07-09 23:20:23 +0800

    完成菜单的查询全部功能（一级列表和二级列表的切换展示）

--- src/pages/category/category.jsx
+++ src/pages/category/category.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Card, Button, Table, message } from 'antd'
+import { Card, Button, Table,Icon, message } from 'antd'
 import LinkButton from "../../components/link-button"
 import { reqCategorys } from '../../api/ajax'
 
@@ -26,9 +26,10 @@ export default class Category extends Component {
                 title: '操作',
                 dataIndex: '',
                 width: 300,
-                render: (category) => <span>
+                render: (category) => this.state.parentId === "0" ?(<span>
                     <LinkButton>修改分类</LinkButton>
-                    <LinkButton onClick={() => this.showSubCategorys(category)}>查看子分类</LinkButton></span>,
+                    <LinkButton onClick={() => this.showSubCategorys(category)}>查看子分类</LinkButton></span>) :(<span>
+                    <LinkButton>修改分类</LinkButton></span>)
             },
         ]
     }
@@ -36,13 +37,13 @@ export default class Category extends Component {
     /*
         查询分类数据：一级列表/二级子列表
     */
-    getCategorys = async () => {
+    getCategorys = async() => {
         this.setState({ loading: true })
         const { parentId } = this.state
         const result = await reqCategorys(parentId)
         this.setState({ loading: false })
         if (result.status === 0) {
-            if (parentId === 0) {
+            if (parentId === '0') {
                 const categorys = result.data
                 this.setState({
                     categorys
@@ -69,9 +70,19 @@ export default class Category extends Component {
             {
                 parentId: category._id,
                 parentName: category.name
+            },()=>{
+                this.getCategorys()
             }
         )
     }
+
+    /**
+     * 回到一级列表
+     */
+    returnFirstPage = ()=>{
+        this.setState({parentId:'0'})
+    }
+
 
     UNSAFE_componentWillMount() {
         this.initColumn()
@@ -84,16 +95,16 @@ export default class Category extends Component {
 
 
     render() {
-
-        //表头数据             
-        const categoryTitle = '一级分类列表'
-
         //状态数据
-        const { categorys, loading } = this.state
+        const { categorys, loading ,subCategorys,parentId,parentName} = this.state
+                    
+        const categoryTitle = parentName?(<span><LinkButton onClick={this.returnFirstPage}>一级分类列表</LinkButton><Icon type="arrow-right" style={{marginRight:12}}/>{parentName}</span>):'一级分类列表'
+
+        
 
         return (
             <Card title={categoryTitle} extra={<Button type='primary' icon="plus">添加</Button>} >
-                <Table dataSource={categorys} columns={this.columns}
+                <Table dataSource={parentId==='0' ? categorys:subCategorys} columns={this.columns}
                     rowKey='_id'
                     pagination={
                         {
