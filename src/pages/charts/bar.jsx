ee368b083c77467216df1295bbbaeb944de9c8fe
zhengqixiang <937084889@qq.com>
2021-07-18 11:29:24 +0800

    使用Echarts完成数据可视化的演示

--- src/pages/charts/bar.jsx
+++ src/pages/charts/bar.jsx
@@ -1,11 +1,52 @@
 import React, { Component } from 'react'
-
+import ReactECharts from 'echarts-for-react'
+import { Button, Card } from 'antd'
 export default class Bar extends Component {
+
+    state={
+        products:[120, 200, 150, 80, 70, 110, 130],
+        storage:[100, 150, 170, 50, 90, 140, 80]
+    }
+
+    update = () =>{
+        this.setState(state => ({
+            products:state.products.map(c => c+10),
+            storage:state.storage.reduce((pre,c)=>{
+                pre.push(c-10)
+                return pre
+            },[])
+        }))
+    }
+
+    getOption = (products,storage)=>{
+        return  {
+            xAxis: {
+                data: ['星期一', '星期二', '星期三', '星期四', '星期五', '星期六', '星期日']
+            },
+            yAxis: {},
+            legend: {
+                data:['销量','库存']
+            },
+            series: [{
+                name:'销量',
+                data: products,
+                type: 'bar'
+            },{
+                name:'库存',
+                data: storage,
+                type: 'bar'
+            }]
+        }
+    }
+
     render() {
+        const {products,storage} =this.state
         return (
-            <div>
-                Bar...
-            </div>
+            <Card title={<Button type='primary' onClick={this.update}>更新</Button>}>
+                <Card title='柱状图'>
+                    <ReactECharts option={this.getOption(products,storage)} />
+                </Card>
+            </Card>
         )
     }
 }
